d44e3ee8039c31045ea136801ccc81b02430e6ec
Alan <alan.rogers@nst.nintendo.com>
2014-01-27 14:27:53 -0800

    Set environment so update-time.sh works as root

--- update-time.sh
+++ update-time.sh
@@ -4,4 +4,10 @@
 # instead of ntp since ntp is blocked over the proxy.
 #
 
+# If run as sudo or root, set up my normal environment
+if [ "$(id -u)" == "0" ]; then
+    export HOME="/Users/Alan"
+    source ~/.bashrc
+fi
+
 sudo date -s "$(curl -I "http://www.google.com/" 2>&1 | grep -E '^[[:space:]]*[dD]ate:' | sed 's/^[[:space:]]*[dD]ate:[[:space:]]*//' | head -1l | awk '{print $1, $3, $2,  $5 ,"GMT", $4 }' | sed 's/,//')"
