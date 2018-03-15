f633e5eb5ef14587bab82ef12fabd799eff6121c
Alan Rogers <alanrogers@google.com>
2018-03-15 20:26:14 +0900

    Add script to tag files with EXIF keywords

---
+++ Photos/tag-keywords.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#
+# tag-keywords.sh
+# Alan Rogers
+# 2018/03/15
+#
+# Adds MacOS Finder tags to files based on EXIF/IPTC keywords.
+#
+# Usage: tag-keywords.sh INPUT_DIRECTORY
+#
+
+# First argument is the directory in which to rename images
+if [ -z "$1" ]; then
+  echo -e ""
+  echo -e "USAGE: exif-to-modified.sh INPUT_DIRECTORY"
+  echo -e ""
+  exit 1
+else
+  DIR=$1
+fi
+
+if ! command -v exiftool > /dev/null; then
+  echo -e ""
+  echo -e "\x1B[00;31mERROR\x1B[00m: Please install exiftool."
+  echo -e ""
+  exit 1
+fi
+
+if ! command -v tag > /dev/null; then
+  echo -e ""
+  echo -e "\x1B[00;31mERROR\x1B[00m: Please install tag."
+  echo -e ""
+  exit 1
+fi
+
+# Extract all keywords with exiftool and tag with tag
+if ls "${DIR}"/*.jpg 1> /dev/null 2>&1; then
+  for FILE in "${DIR}"/*.jpg
+  do
+    KEYWORDS="$(exiftool -s3 -keywords ${FILE})"
+    echo -e "\x1B[00;33m${FILE}\x1B[00m ${KEYWORDS}"
+    IFS=","
+    for KEYWORD in $KEYWORDS
+    do
+      tag --add "${KEYWORD}" "${FILE}"
+    done
+    unset IFS
+  done
+fi
+
+exit 0
