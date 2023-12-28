496c1a46bbca48d9e3206b9511ca16d14b365669
Alan Rogers <alanrogers@google.com>
2023-12-28 19:30:47 +0900

    Updates to finalize-photos.sh
    I don't remember the details

--- Photos/finalize-photos.sh
+++ Photos/finalize-photos.sh
@@ -45,44 +45,54 @@ if [ ${#heic_files[@]} -gt 0 ]; then
   readarray -t sorted_heic_files < <(printf '%s\n' "${heic_files[@]}" | sort)
 
   import_result=$(osascript <<EOD
-    set heicFilesStr to "$(printf '%s\n' "${sorted_heic_files[@]}")"
-    set heicFiles to the paragraphs of heicFilesStr
-    set photosApp to (path to application "Photos")
+set heicFilesStr to "$(printf '%s\n' "${sorted_heic_files[@]}")"
+set heicFiles to the paragraphs of heicFilesStr
+set photosApp to (path to application "Photos")
 
-    tell application "Photos"
-      set importedFiles to {}
-      set photoFiles to {}
-      repeat with heicFile in heicFiles
-        set posixPath to heicFile as string
-        set photoFile to POSIX file posixPath as alias
-        set end of photoFiles to photoFile
-      end repeat
-      set importedFiles to import photoFiles
+tell application "Photos"
+	set photoFiles to {}
+	repeat with heicFile in heicFiles
+		set posixPath to heicFile as string
+		set photoFile to POSIX file posixPath as alias
+		set end of photoFiles to photoFile
+	end repeat
+	set importedFiles to import photoFiles
 
-      set favoriteKeyword to "♡"
-      set frameKeyword to "Frame"
-      set frameAlbum to container "Frame"
-      set frameLandscapeAlbum to container "Frame Landscape"
+	set favoriteKeyword to "♡"
+	set frameKeyword to "Frame"
+	set frameAlbum to first album whose name is "Frame"
+	set frameLandscapeAlbum to first album whose name is "Frame Landscape"
+	
+	set frameMediaList to {}
+	set frameLandscapeMediaList to {}
+	
+	repeat with mediaItem in importedFiles
+		set mediaKeywords to keywords of mediaItem
+		if favoriteKeyword is in mediaKeywords then
+			set favorite of mediaItem to true
+		end if
+		if frameKeyword is in mediaKeywords then
+			set end of frameMediaList to mediaItem
+			if width of mediaItem > height of mediaItem then
+				set end of frameLandscapeMediaList to mediaItem
+			end if
+		end if
+	end repeat
+	
+  if (count of frameMediaList) > 0 then
+      add frameMediaList to frameAlbum
+  end if
+  if (count of frameLandscapeMediaList) > 0 then
+      add frameLandscapeMediaList to frameLandscapeAlbum
+  end if
+	
+end tell
 
-      repeat with mediaItem in importedFiles
-        set mediaKeywords to keywords of mediaItem
-        if favoriteKeyword is in mediaKeywords then
-          set favorite of mediaItem to true
-        end if
-        if frameKeyword is in mediaKeywords then
-          add mediaItem to frameAlbum
-          if width of mediaItem > height of mediaItem then
-            add mediaItem to frameLandscapeAlbum
-          end if
-        end if
-      end repeat
-    end tell
-
-    if (count of importedFiles) = (count of heicFiles) then
-      return "All photos successfully imported"
-    else
-      return "An error occurred while importing photos to Apple Photos."
-    end if
+if (count of importedFiles) = (count of heicFiles) then
+	return "All photos successfully imported"
+else
+	return "An error occurred while importing photos to Apple Photos."
+end if
 EOD
 )
 echo -e "$import_result"
